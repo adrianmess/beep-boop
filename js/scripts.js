ed5d5a6307908948d0c7f697fa2509063f636ba3
Adrian Messado <adrian.messado@gmail.com>
2018-03-30 14:17:40 -0700

    seperate user logic and business logic | confirm logic works as expected

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 //Business - Back-End Logic
 
+var compute = function(string) {
+
+  if (string.match(/\D+/g)) {
+    alert("Only numbers are permitted. No Soup For YOU!")
+  }
+
+
+  // If user input
+   else if (string % 3 === 0) {
+    alert("I'm sorry, Dave. I'm afraid I can't do that.")
+  }
+
+   else if (string.match(/[1]/g)) {
+     alert("Boop!")
+   }
+
+   else if (string.match(/[0]/g)) {
+     alert("Beep!")
+     }
+     else {
+       alert(randomNumb)
+     }
+
+};
+
 
 
 //User - Front-End Logic
@@ -9,42 +34,7 @@ $(document).ready(function(){
     // debugger;
   var string = $("#input").val();
   var randomNumb= Math.floor(Math.random() * string);
-
- // console.log(regex1.test(string));
-
-// if userinput contains characters that are not digits
- if (string.match(/\D+/g)) {
-   alert("Only numbers are permitted. No Soup For YOU!")
- }
-
-
-// If user input
-  else if (string % 3 === 0) {
-   alert("I'm sorry, Dave. I'm afraid I can't do that.")
- }
-  // else if (string.match(/\d/g)){
-  //
-  //   }
-  else if (string.match(/[1]/g)) {
-    alert("Boop!")
-  }
-
-  else if (string.match(/[0]/g)) {
-    alert("Beep!")
-    }
-    else {
-      alert(randomNumb)
-    }
+  var result = compute(string) 
 
 });
 });
-// if (string.match(/\d/g)) {
-// } else {
-//   alert("Only numbers are permitted. No Soup For YOU!")
-// }
-
- // if (string.match(/\d/g)) {
- //    alert ("true")
- // } else {
- //   alert ('false')
- // }
