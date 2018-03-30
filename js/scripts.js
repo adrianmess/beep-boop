1e69170c3a7421c75fda9fb4a81657af78e272f8
Adrian Messado <adrian.messado@gmail.com>
2018-03-30 11:56:38 -0700

    create regular expression to verify if input is a number or character

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,24 @@
 
 
 
+
+
+
+
+
+
+
 //User - Front-End Logic
 $(document).ready(function(){
-  
+  $(".formOne").submit(function(event) {
+    event.preventDefault();
+    // debugger;
+  var string = $("#input").val();
+  var regex1 = RegExp(/\d/g);
+ console.log(regex1.test(string));
+
+
+
+});
+
 });
