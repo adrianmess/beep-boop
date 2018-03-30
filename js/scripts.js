e656bb668d3246940652725280e639435e5db2ca
Adrian Messado <adrian.messado@gmail.com>
2018-03-30 14:32:01 -0700

    adjust function to output results to website, confirm previous output is replaced when new input is added and submit button is selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,39 @@
+//--------------------------||
 //Business - Back-End Logic
+//--------------------------||
 
 var compute = function(string) {
-
   if (string.match(/\D+/g)) {
-    alert("Only numbers are permitted. No Soup For YOU!")
+    $("#h2").text("Only numbers are permitted. No Soup For YOU!")
   }
-
-
-  // If user input
-   else if (string % 3 === 0) {
-    alert("I'm sorry, Dave. I'm afraid I can't do that.")
+  else if (string % 3 === 0) {
+    $("#h2").text("I'm sorry, Dave. I'm afraid I can't do that.")
   }
+  else if (string.match(/[1]/g)) {
+    $("#h2").text("Boop!")
+  }
+  else if (string.match(/[0]/g)) {
+    $("#h2").text("Boop!")
 
-   else if (string.match(/[1]/g)) {
-     alert("Boop!")
-   }
-
-   else if (string.match(/[0]/g)) {
-     alert("Beep!")
-     }
-     else {
-       alert(randomNumb)
-     }
-
+  }
+  else {
+    var randomNumb= Math.floor(Math.random() * string);
+    $("#h2").text(randomNumb)
+  }
 };
 
 
-
+//--------------------------||
 //User - Front-End Logic
+//--------------------------||
+
 $(document).ready(function(){
   $(".formOne").submit(function(event) {
     event.preventDefault();
     // debugger;
-  var string = $("#input").val();
-  var randomNumb= Math.floor(Math.random() * string);
-  var result = compute(string) 
-
-});
+    var string = $("#input").val();
+    // var randomNumb= Math.floor(Math.random() * string);
+    var result = compute(string);
+    $("#h2").text(result);
+  });
 });
