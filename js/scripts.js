3bb448ac629b2ef0fcfcdc8e7102c1af1149c642
Adrian Messado <adrian.messado@gmail.com>
2018-03-30 13:33:46 -0700

    add if statement to return Boop! if input 1, if statement to return 'i'm sorry dave..., and return beep if 0

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,43 @@
 
 
 
-
-
-
-
-
-
-
-
 //User - Front-End Logic
 $(document).ready(function(){
   $(".formOne").submit(function(event) {
     event.preventDefault();
     // debugger;
   var string = $("#input").val();
-  // var regex1 = RegExp(/\d/g);
+
  // console.log(regex1.test(string));
 
- if (string.match(/\d/g)) {
-    alert ("true")
- } else {
-   alert ('false')
+// if userinput contains characters that are not digits
+ if (string.match(/[a-zA-Z]+/g)) {
+   alert("Only numbers are permitted. No Soup For YOU!")
  }
 
+// If user input
+ if (string.match(/\d/g)){
+   if (string.match(/[0]/g)) {
+     alert("Beep!")
+    }
+    else if (string.match(/[1]/g)) {
+      alert("Boop!")
+    }
+    else if (string % 3 === 0) {
+      alert("I'm sorry, Dave. I'm afraid I can't do that.")
+    }
 
-
+ }
 });
 
 });
+// if (string.match(/\d/g)) {
+// } else {
+//   alert("Only numbers are permitted. No Soup For YOU!")
+// }
+
+ // if (string.match(/\d/g)) {
+ //    alert ("true")
+ // } else {
+ //   alert ('false')
+ // }
