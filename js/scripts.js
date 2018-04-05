02fe62f01f9791f525ef3486843f46b5964598ab
Adrian Messado <adrian.messado@gmail.com>
2018-04-04 21:25:25 -0700

    adjusted front-end logic and html tags to get input from user  to store as variable

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,15 @@
 //--------------------------||
 //User - Front-End Logic
 //--------------------------||
-$(document).ready(function(){
-  $('.formOne').submit(function(event){
-    event.preventDefault();
-    var numberInput = $("#input").val();
-    var numberInt = parseInt(numberInput)
-    
-  })
 
+$(document).ready(function(){
+  $("form.formOne").submit(function(event){
+    event.preventDefault();
+    var numberInput = $("input#input").val();
+    console.log(numberInput)
+    var numberInt = parseInt(numberInput)
+    console.log(numberInt)
+    // console.log(numberInput);
+  });
+// console.log(numberInt);
 });
