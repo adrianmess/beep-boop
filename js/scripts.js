826677de40932fd05fe7d6fb806c147f02977070
Adrian Messado <adrian.messado@gmail.com>
2018-04-04 23:37:28 -0700

    expand on loop within verifyNumber function to itterate array and alert if there is a number divisible by 3.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,37 @@
 //Business - Back-End Logic
 //--------------------------||
 
+//Determine if input is a number or not. {
 
 
+
+function verifyNumber(numberInput) {
+  if (isNaN(numberInput)) {
+
+    return "Only numbers are permitted. No Soup For YOU!";
+  //   console.log(numberInputInt)
+}
+ else
+     var numberIndex = numberInput.split('');{
+     // var numberIndex =
+   }
+
+      for (i = 0 ; i < numberIndex.length; i++) {
+        if (numberIndex[i] % 3 === 0) {
+          alert("division by 33 working?")
+        }
+      }
+   return numberIndex; {
+
+}
+
+}
+
+
+   // (numberInputInt.match(/\D+/g))
+   //  return "Only numbers are permitted. No Soup For YOU!"
+
+// console.log(verifynumnber(numberInputInt))
 
 //--------------------------||
 //User - Front-End Logic
@@ -12,11 +41,15 @@
 $(document).ready(function(){
   $("form.formOne").submit(function(event){
     event.preventDefault();
-    var numberInput = $("input#input").val();
-    console.log(numberInput)
-    var numberInt = parseInt(numberInput)
-    console.log(numberInt)
-    // console.log(numberInput);
-  });
-// console.log(numberInt);
+    var numberInput = $("#userInput").val();
+    console.log(numberInput);
+    console.log(typeof numberInput);
+    var numberInputInt = parseInt(numberInput);
+    console.log(numberInputInt);
+    console.log(typeof numberInputInt);
+    var output = verifyNumber(numberInput);
+    console.log(output);
+    // $("#output").text(verifyNumber(numberInputInt))
+  })
+
 });
