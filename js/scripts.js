5c114fbe773fd123d1509659bd626cb898e6da08
Adrian Messado <adrian.messado@gmail.com>
2018-04-06 05:07:00 -0700

    create and test functon and loops in seperate files. cleaup comments in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,6 @@
 
 //Determine if input is a number or not. {
 
-
-
 function verifyNumber(numberInput) {
   if (isNaN(numberInput)) {
 
@@ -13,79 +11,14 @@ function verifyNumber(numberInput) {
   //   console.log(numberInputInt)
 }
  else
+
      var numberIndex = numberInput.split('');{
-       var newArray = numberIndex.slice();
-       console.log(newArray)
- //replace element?
-       // while (numberIndex.indexof.numberIndex == numberIndex % 3 === 0); {
-       //   numberIndex.splice(numberIndex.indexof(numberIndex),1,"Sorry test");
+       console.log(numberIndex)
+       var arrayOfNumbers = numberIndex.map(Number);
+       console.log(arrayOfNumbers)
 
        }
-       for (var i = 0, l = numberIndex.length; i < l; i++) {
-         if (newArray[i] % 3 === 0) {
-           newArray.splice(newArray.indexOf("3"),1,"**");
-           console.log(newArray)
-         }
-       }
-
-
-     // var numberIndex =
-     // var index = function modulo(numberIndex) {
-     //    (i = 0; i < index.length; 1++)
-     //   if (i % 3 === 0) {
-     //    i.replace()}
-     //
-     //   }
-     console.log(numberIndex)
-   }
-
-
-
-
-
-//traverse array
-      // var super = function(numberIndex){
-      //   (i % 3 === 0)
-      // }
-      // console.log(numberIndex)
-      // numberIndex.forEach( (divThree) {
-      //   var i = divThree.
-      //   var modulo = i % 3 === 0
-      // } i; i % 3 === 0; ; i++) {
-
-
-
-// }
-// if ( i % 3 === 0) {
-//
-// }
-
-        // console.log(numberIndex.length)
-
-        // console.log(i)
-        // console.log(numberIndex)
-      //   //verify if number divisible by 3
-      //   if (i % 3 === 0) {
-      //     console.log("divisible by 3 working")
-      //     // alert("division by 33 working?")
-
-        // console.log(map1)
-
-
-        // console.log(numberIndex);
-
-   // return numberIndex;
-//    console.log(numberIndex); {
-//
-// }
-//
-// }
-
-
-   // (numberInputInt.match(/\D+/g))
-   //  return "Only numbers are permitted. No Soup For YOU!"
-
-// console.log(verifynumnber(numberInputInt))
+     };
 
 //--------------------------||
 //User - Front-End Logic
