ca2eae09d6da134fc6decffd39036f05b5177888
Adrian Messado <adrian.messado@gmail.com>
2018-04-08 14:58:39 -0700

    add while statement to return response if non number is entered

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,14 @@
 function beepBoop(numberInput) {
   var outPut = []
   var numberInputInt = parseInt(numberInput);
+  var noSoup = "You must enter number! No Soup for YOU!!!"
 
+  while (isNaN(numberInputInt)) {
+    return noSoup;
+  }
+  
   for (var i = 1; i <= numberInputInt; i++ ) {
-    if (i % 3 === 0 && i !== 0) {
+     if (i % 3 === 0 && i !== 0) {
       outPut.push("I'm sorry Dave, I can't do that");
     } else if (i.toString().includes("1")) {
       outPut.push("BOOP!");
@@ -31,6 +36,7 @@ $(document).ready(function(){
   $("form.formOne").submit(function(event){
     event.preventDefault();
     var numberInput = $("#userInput").val();
+    console.log(typeof numberInput)
     var beepBoopOut = beepBoop(numberInput);
     $("#output").text(beepBoopOut)
   })
